64bd011c15572ffaaeaa881b4b1f7f9c01c8a7cf
Martin Zumaya <sstereo.vision@gmail.com>
2017-05-03 17:51:39 -0500

    testing

---
+++ test/data_structures.jl
@@ -0,0 +1,27 @@
+
+using Plots
+
+gr()
+
+# number of walkers
+N = 256
+
+# bias intensity
+ϵ = 0.2
+
+# initalization space
+Lx = 15
+Ly = 15
+
+# inital particles positions
+x_pos = rand(1:2Lx, N)
+y_pos = rand(1:2Ly, N)
+
+x_pos = rand(1:Lx, N)
+y_pos = rand(1:Ly, N)
+
+scatter(x_pos, y_pos, leg = false)
+
+pos = sparse(x_pos, y_pos, ones(N))
+
+rowvals(pos)
