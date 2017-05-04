5494051b4b190bff3b7c72ede90cf84d0fc06ea1
Martin Zumaya <sstereo.vision@gmail.com>
2017-05-04 11:46:17 -0500

    test on border detection

--- test/data_structures.jl
+++ test/data_structures.jl
@@ -1,7 +1,37 @@
+### ============== ### ============== ###
+## Collective Motion in Lattice Systems
+## Martin Zumaya Hernandez
+## 05 / 2017
+### ============== ### ============== ### 
+
+
+### ================================== ###
+
+function get_borders(sp_mat)
+
+    x_b = Array{Int}[]
+    y_b = Array{Int}[]
+
+    for i in 1:size(sp_mat, 2)
+        v_range = nzrange(sp_mat, i)
+
+        x = collect(extrema(rowvals(sp_mat)[v_range]))
+        y = fill(i, length(x))
+
+        push!(x_b, x)
+        push!(y_b, y)
+
+    end
+    return vcat(x_b...), vcat(y_b...)
+end
+
+### ================================== ###
 
 using Plots
 
 gr()
+
+### ================================== ###
 
 # number of walkers
 N = 256
@@ -9,20 +39,26 @@ N = 256
 # bias intensity
 ϵ = 0.2
 
+# initial density
+ρ_0 = 0.5
+
 # initalization space
-Lx = 15
-Ly = 15
+L = convert(Int, ceil(sqrt(N/ρ_0)))
 
+### ================================== ###
 # inital particles positions
-x_pos = rand(1:2Lx, N)
-y_pos = rand(1:2Ly, N)
+x_pos = rand(1:L, N)
+y_pos = rand(1:L, N)
 
-x_pos = rand(1:Lx, N)
-y_pos = rand(1:Ly, N)
+part_plot = scatter(x_pos, y_pos, leg = false)
+border_plot = scatter()
 
-scatter(x_pos, y_pos, leg = false)
+pos = sparse(x_pos, y_pos, ones(Int,N))
 
-pos = sparse(x_pos, y_pos, ones(N))
+hx_b, hy_b = get_borders(pos)
+vy_b, vx_b = get_borders(transpose(pos))
 
-rowvals(pos)
-nzrange(pos, 1)
+scatter!(border_plot, hx_b, hy_b, leg = false)
+scatter!(border_plot, vx_b, vy_b, leg = false)
+
+plot(part_plot, border_plot, layout = @layout [a b])
