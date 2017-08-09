69874429a3713b0eb3a49882cc0aa157406867b5
martinzh <sstereo.vision@gmail.com>
2017-08-09 11:44:12 -0500

    all epsilon for same N script

---
+++ test/all_e_N.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+N="$1"
+T="$2"
+
+bash reps_1D.sh $N 0.0 $T
+bash reps_1D.sh $N 0.1 $T
+bash reps_1D.sh $N 0.25 $T
+bash reps_1D.sh $N 0.5 $T
+bash reps_1D.sh $N 0.75 $T
+bash reps_1D.sh $N 1.0 $T
