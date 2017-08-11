1b972b6a35b32182990b4948d86f3e0badcba660
martinzh <sstereo.vision@gmail.com>
2017-08-11 16:03:20 -0500

    added wait cmd to batch script

--- test/all_e_N_2D.sh
+++ test/all_e_N_2D.sh
@@ -4,10 +4,17 @@ N="$1"
 T="$2"
 
 bash reps_2D.sh $N 0.0 $T
+wait
 bash reps_2D.sh $N 0.01 $T
+wait
 bash reps_2D.sh $N 0.05 $T
+wait
 bash reps_2D.sh $N 0.1 $T
+wait
 bash reps_2D.sh $N 0.25 $T
+wait
 bash reps_2D.sh $N 0.5 $T
+wait
 bash reps_2D.sh $N 0.75 $T
+wait
 bash reps_2D.sh $N 1.0 $T
