2765d01873e7186dc5bb700e8ce1fb05bfbccdfe
martinzh <sstereo.vision@gmail.com>
2017-08-30 13:41:47 -0500

    changed script name

--- test/all_exp_1D_N.sh
+++ test/all_exp_1D_N.sh
@@ -2,11 +2,11 @@
 
 N="$2"
 
-bash exp_1D.sh LS_1D $N 0.0 
-bash exp_1D.sh LS_1D $N 0.01
-bash exp_1D.sh LS_1D $N 0.05
-bash exp_1D.sh LS_1D $N 0.1
-bash exp_1D.sh LS_1D $N 0.25
-bash exp_1D.sh LS_1D $N 0.5
-bash exp_1D.sh LS_1D $N 0.75
-bash exp_1D.sh LS_1D $N 1.0
+nohup julia exp_1D.jl LS_1D $N 0.0  &
+nohup julia exp_1D.jl LS_1D $N 0.01 &
+nohup julia exp_1D.jl LS_1D $N 0.05 &
+nohup julia exp_1D.jl LS_1D $N 0.1 &
+nohup julia exp_1D.jl LS_1D $N 0.25 &
+nohup julia exp_1D.jl LS_1D $N 0.5 &
+nohup julia exp_1D.jl LS_1D $N 0.75 &
+nohup julia exp_1D.jl LS_1D $N 1.0 &
