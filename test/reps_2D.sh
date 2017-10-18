8a3df4ef42d10b2bde90c019488c27753dc6bb93
Martin Zumaya <sstereo.vision@gmail.com>
2017-10-18 17:23:38 -0500

    typo

--- test/reps_2D.sh
+++ test/reps_2D.sh
@@ -6,14 +6,14 @@ T="$3"
 
 for i in {1..20}
 do
-    nohup /Users/mzumaya/Code/julia/julia 2D_test.jl $N $e $T $i &
+    nohup /home/martin/Code/julia/julia 2D_test.jl $N $e $T $i &
 done
 
 wait
 
 for i in {21..40}
 do
-    time nohup julia 2D_test.jl $N $e $T $i &
+    nohup /home/martin/Code/julia/julia 2D_test.jl $N $e $T $i &
 done
 
 # wait
