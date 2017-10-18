2682bb519fda9a8926ed921fdada7e2d2fa9befe
Martin Zumaya <sstereo.vision@gmail.com>
2017-10-18 17:11:20 -0500

    mods

--- test/reps_2D.sh
+++ test/reps_2D.sh
@@ -4,18 +4,18 @@ N="$1"
 e="$2"
 T="$3"
 
-for i in {1..12}
+for i in {1..20}
 do
     nohup /Users/mzumaya/Code/julia/julia 2D_test.jl $N $e $T $i &
 done
 
-# wait
-#
-# for i in {21..40}
-# do
-#     time nohup julia 2D_test.jl $N $e $T $i &
-# done
-#
+wait
+
+for i in {21..40}
+do
+    time nohup julia 2D_test.jl $N $e $T $i &
+done
+
 # wait
 #
 # for i in {41..60}
